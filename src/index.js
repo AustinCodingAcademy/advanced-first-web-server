e4c0d7f0fbd448ab12f0e17ee6fd17f47b76ab86
ssfiero <ssfiero@yahoo.com>
2017-02-13 20:55:12 -0600

    Web Server - Logged success message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// Your server code here...
+// Import the express module
+import express from 'express';
+
+// Create a new instance of express
+const app = express();
+
+// Declare our route
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+// Set our port to server the application on
+const PORT = 3001;
+
+// Tell our instance of express to listen to request made on our port
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Error!', err);
+  }
+
+  return console.log('Listening on: http://localhost:' + PORT);
+});
