2c65814929d062f752ac04f045fa3549a035b185
nicolemichelle88 <daugereaux@gmail.com>
2017-02-14 20:03:21 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// Your server code here...
+import express from 'express';
+
+// create a new instance of express
+const app = express();
+
+// declare our route
+
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+// set our port to server the application on
+const PORT = 3001;
+
+// tell our istace of express to listen to request made on our port
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Error!', err);
+  }
+
+  return console.log('Listening on: http://localhost:' + PORT);
+});
