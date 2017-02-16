e50d4fb6a81d1be9c5be50ef0143b42afac776a3
Bradley Cuccio <bradleycuccio@gmail.com>
2017-02-16 14:14:18 -0600

    first web server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// Your server code here...
+// import the express module
+import express from 'express';
+
+// create a new instance of express
+const app = express();
+
+// declare our route
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+// set our port to server the application on
+const PORT = 3001;
+
+// tell our instance of express to listen to request made on our port
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Error!', err);
+  }
+
+  return console.log('Listening on: http://localhost:' + PORT);
+});
