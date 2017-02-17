ba256987d47a442b580a2c6bfac30edc1eb18190
dmencha <dmencha@rMB.local>
2017-02-16 19:25:45 -0600

    Express first server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // Your server code here...
+import express from 'express';
+
+const app = express();
+
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('Error!', err);
+  }
+  return console.log('Listening on: http://localhost:' + PORT);
+
+});
