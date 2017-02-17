64e75d59763d7240d7d2243c6511182a3ebdd8d7
Austin Shipp <austinshipp@yahoo.com>
2017-02-17 14:42:12 -0600

    Routing and MongoDB setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
-// Your server code here...
+import express from 'express';
+import mongoose from 'mongoose';
+import ContactModel from './models/ContactModel';
+import bodyParser from 'body-parser';
+
+const app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/contact-list');
+
+app.use(bodyParser.json());
+
+app.get('/contacts', (req, resp) => {
+  ContactModel.find({}).exec()
+    .then(contacts => {
+      return resp.json(contacts);
+    })
+    .catch(err => {
+      console.log(`Error! ${err}`);
+    });
+});
+
+app.get('/contacts/:_id', (req, resp) => {
+  ContactModel.findById(req.params._id).exec()
+    .then(contact => {
+      return resp.json(contact);
+    })
+    .catch(err => {
+      console.log(`Error! ${err}`);
+    });
+});
+
+app.post('/contacts', (req, resp) => {
+  const contact = new ContactModel({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    avatar: req.body.avatar
+  });
+
+  contact.save()
+    .then(newContact => {
+      return resp.json(newContact);
+    });
+});
+
+app.delete('/contacts/:_id', (req, res) => {
+  ContactModel.findByIdAndRemove(req.params._id).exec()
+    .then(contact => {
+      return res.json(contact);
+    })
+    .catch(err => {
+      console.log(`Error! ${err}`);
+    });
+});
+
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('ERROR!', err);
+  }
+
+  return console.log('Listening on: http://localhost:' + PORT);
+});
