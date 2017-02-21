b412ec0a87c75a8106c69d2b843f76cd0c65399d
Jonathan Soape <jonathansoape@gmail.com>
2017-02-21 14:40:18 -0600

    Added REST requests to contact list api

--- src/index.js
+++ src/index.js
@@ -1,26 +1,85 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
-// Instantiate
 const app = express();
 
-// Declare our Get /contacts route
-app.get('/contacts', (request, response) => {
-  const contacts = [
-    {
-      _id: 1,
-      name: 'Nugget Soape',
-      occupation: 'Ankle Biter',
-      avatar: 'https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-9/13879202_10107942913699150_5756944923215245156_n.jpg?oh=db34837500d67ba28c5160ca6b35a2b9&oe=59113BBF'
-    },
-    {
-      _id: 3,
-      name: 'Fizzgig',
-      occupation: 'Gelfling Companion',
-      avatar: 'http://vignette1.wikia.nocookie.net/darkcrystal/images/8/81/Fizzgig.jpg/revision/latest?cb=20080506205401'
-    }
-  ];
+// Connect to mongoose db
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/contact-list');
 
-  return response.json(contacts);
+app.use(bodyParser.json());
+
+const ContactModel = require('./models/ContactModel');
+
+// Declare GET /contacts route
+app.get('/contacts', function (request, response) {
+  ContactModel.find({}).exec()
+    .then(contacts => {
+      return response.json(contacts);
+    })
+    .catch(err => {
+      return console.log(err);
+    });
+});
+
+app.get('/contacts/:_id', function (request, response) {
+  ContactModel.findById(request.params._id).exec()
+    .then(contact => {
+      return response.json(contact);
+    })
+    .catch(err => {
+      return console.log(err);
+    });
+});
+
+app.delete('/contacts/:_id', function (request, response) {
+  ContactModel.findByIdAndRemove(request.params._id).exec()
+    .then(contact => {
+      return response.json(contact);
+    })
+    .catch(err => {
+      return console.log(err);
+    });
+});
+
+app.post('/contacts', function (request, response) {
+  // Create new instance of ContactModel
+  // Grap attributes from request.body object (from body-parser)
+  const contact = new ContactModel({
+    name: request.body.name,
+    occupation: request.body.occupation,
+    avatar: request.body.avatar,
+  });
+
+  // Save new contactcontact.save()
+  contact.save()
+    // Then return the new contact
+    .then(() => {
+      return response.json(contact);
+    })
+    .catch(err => {
+      return console.log(err);
+    });
+});
+
+app.put('/contacts/:_id', function (request, response) {
+  ContactModel.findById(request.params._id).exec()
+   .then(contact => {
+     // Set the attributes on the model from the request.body
+     // OR if nothing is received, whatever the current value is
+     contact.name = request.body.name || contact.name;
+     contact.occupation = request.body.occupation || contact.occupation;
+     contact.avatar = request.body.avatar || contact.avatar;
+
+     return contact.save();
+   })
+   .then(contact => {
+     return response.json(contact);
+   })
+   .catch(err => {
+     return console.log(err);
+   });
 });
 
 // Declare the route
