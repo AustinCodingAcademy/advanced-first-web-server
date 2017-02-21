e7cda352dd1e5f52a58963fd90bddabf91ac2978
Jonathan Soape <jonathansoape@gmail.com>
2017-02-21 15:45:46 -0600

    Added error handler middleware

--- src/index.js
+++ src/index.js
@@ -13,37 +13,37 @@ app.use(bodyParser.json());
 const ContactModel = require('./models/ContactModel');
 
 // Declare GET /contacts route
-app.get('/contacts', function (request, response) {
+app.get('/contacts', function (request, response, next) {
   ContactModel.find({}).exec()
     .then(contacts => {
       return response.json(contacts);
     })
     .catch(err => {
-      return console.log(err);
+      return next(err);
     });
 });
 
-app.get('/contacts/:_id', function (request, response) {
+app.get('/contacts/:_id', function (request, response, next) {
   ContactModel.findById(request.params._id).exec()
     .then(contact => {
       return response.json(contact);
     })
     .catch(err => {
-      return console.log(err);
+      return next(err);
     });
 });
 
-app.delete('/contacts/:_id', function (request, response) {
+app.delete('/contacts/:_id', function (request, response, next) {
   ContactModel.findByIdAndRemove(request.params._id).exec()
     .then(contact => {
       return response.json(contact);
     })
     .catch(err => {
-      return console.log(err);
+      return next(err);
     });
 });
 
-app.post('/contacts', function (request, response) {
+app.post('/contacts', function (request, response, next) {
   // Create new instance of ContactModel
   // Grap attributes from request.body object (from body-parser)
   const contact = new ContactModel({
@@ -59,11 +59,11 @@ app.post('/contacts', function (request, response) {
       return response.json(contact);
     })
     .catch(err => {
-      return console.log(err);
+      return next(err);
     });
 });
 
-app.put('/contacts/:_id', function (request, response) {
+app.put('/contacts/:_id', function (request, response, next) {
   ContactModel.findById(request.params._id).exec()
    .then(contact => {
      // Set the attributes on the model from the request.body
@@ -78,13 +78,18 @@ app.put('/contacts/:_id', function (request, response) {
      return response.json(contact);
    })
    .catch(err => {
-     return console.log(err);
+     return next(err);
    });
 });
 
 // Declare the route
 app.all('/*', (request, response) => {
   return response.send(request.params['0']);
+});
+
+// Error handler middleware
+app.use(function (err, request, response, next) {
+  return response.status(500).send('You done screwed up. ' + err)
 });
 
 const PORT = 3001;
