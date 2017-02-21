b8a86342a087cf465c432bace08b6d798ffee1b2
Alex Gaw <alexanderjgaw@gmail.com>
2017-02-20 21:08:28 -0600

    First Web Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Your server code here...
+/* eslint no-console: 0 */
+
+import express from 'express';
+
+const app = express();
+
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    return console.log('Error!', error);
+  }
+
+  return console.log('Listening on: http://localhost:' + PORT);
+});
