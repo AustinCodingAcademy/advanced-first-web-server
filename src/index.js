3e3a6d9df73d94dfa364c75346f8ee9e7d665a0b
Austin Shipp <austinshipp@yahoo.com>
2017-02-23 18:58:01 -0600

    Completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import ContactModel from './models/ContactModel';
+import ContactRoutes from './routes/ContactRoutes';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -8,53 +8,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/contact-list');
 
 app.use(bodyParser.json());
-
-app.get('/contacts', (req, resp) => {
-  ContactModel.find({}).exec()
-    .then(contacts => {
-      return resp.json(contacts);
-    })
-    .catch(err => {
-      console.log(`Error! ${err}`);
-    });
-});
-
-app.get('/contacts/:_id', (req, resp) => {
-  ContactModel.findById(req.params._id).exec()
-    .then(contact => {
-      return resp.json(contact);
-    })
-    .catch(err => {
-      console.log(`Error! ${err}`);
-    });
-});
-
-app.post('/contacts', (req, resp) => {
-  const contact = new ContactModel({
-    name: req.body.name,
-    occupation: req.body.occupation,
-    avatar: req.body.avatar
-  });
-
-  contact.save()
-    .then(newContact => {
-      return resp.json(newContact);
-    });
-});
-
-app.delete('/contacts/:_id', (req, res) => {
-  ContactModel.findByIdAndRemove(req.params._id).exec()
-    .then(contact => {
-      return res.json(contact);
-    })
-    .catch(err => {
-      console.log(`Error! ${err}`);
-    });
-});
-
-app.all('/*', (request, response) => {
-  return response.send(request.params['0']);
-});
+app.use(ContactRoutes);
 
 const PORT = 3001;
 
