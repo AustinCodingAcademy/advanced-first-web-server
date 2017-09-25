41f0f4d922478488ae92bfa9141ae1a51b2c9d81
Ericka Emery <ericka@elonandcompany.com>
2017-09-24 19:06:42 -0500

    fixed lint errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // Your server code here...
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.send('you tried to get the route path');
+});
+   
+app.listen(3002, (err) => {
+  if (err) {
+    return console.log('Error', err);
+  }
+});
