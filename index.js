b71defe461440346991f7c57e5833044ddbb17da
Paul Mathis <paul.mathis@gmail.com>
2018-10-24 19:06:52 -0500

    part 2 complete

--- index.js
+++ index.js
@@ -5,59 +5,30 @@ const app = express();
 app.use(express.static("public"));
 
 const users = require("./state").users;
-app.use((req, res, next) => {
-  switch (req.method) {
-    case "GET":
-      switch (req.path) {
-        case "/users":
-          res.send(users);
-          break;
-        case "/users/1":
-          res.send(users[0]);
-          break;
-        default:
-          res.send("Not Found");
-      }
-      break;
-    case "POST":
-      switch (req.path) {
-        case "/users":
-          users.push({
-            _id: 6,
-            name: "Paul",
-            occupation: "developer",
-            avatar:
-              "https://pbs.twimg.com/profile_images/858002219311837185/3hOvqcN1_400x400.jpg"
-          });
-          res.json(users[users.length - 1]);
-          break;
-        default:
-          res.send("Not Found");
-      }
-      break;
-    case "PUT":
-      switch (req.path) {
-        case "/users/1":
-          users[0].name = "bob";
-          res.json(users[0]);
-          break;
-        default:
-          res.send("Not Found");
-      }
-      break;
-    case "DELETE":
-      switch (req.path) {
-        case "/users/1":
-          users.splice(0, 1);
-          res.send("deleted");
-          break;
-        default:
-          res.send("Not Found");
-      }
-      break;
-    default:
-      res.send("Invalid Request");
-  }
+
+app.get("/users", (req, res, next) => {
+  res.send(users);
+});
+app.get("/users/1", (req, res, next) => {
+  res.send(users[0]);
+});
+app.post("/users", (req, res, next) => {
+  users.push({
+    _id: 6,
+    name: "Paul",
+    occupation: "developer",
+    avatar:
+      "https://pbs.twimg.com/profile_images/858002219311837185/3hOvqcN1_400x400.jpg"
+  });
+  res.json(users[users.length - 1]);
+});
+app.put("/users/1", (req, res, next) => {
+  users[0].name = "bob";
+  res.json(users[0]);
+});
+app.get("/users/1", (req, res, next) => {
+  users.splice(0, 1);
+  res.send("deleted");
 });
 
 app.listen(3002, err => {
